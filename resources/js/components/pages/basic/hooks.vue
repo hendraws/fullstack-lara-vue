e092d966c4099297448b051255f526055bc58434
hendrawsa <saputra.hendrawijaya@gmail.com>
2021-01-06 11:53:32 +0700

    persiapan project laravel vue.js

---
+++ resources/js/components/pages/basic/hooks.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h1 ref="myref">Hook Tutorial</h1>
+		<h2>My Name Is {{ name }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			name : "hendra"
+		}
+	},
+	beforeCreate(){
+		console.log(this.name);
+	},
+	created(){
+		// console.log(this.$refs.myref)
+		console.log('data and order function is avilable but not html', this.name)
+	},
+	mounted(){
+		console.log(this.$refs.myref)
+	}
+}
+</script>
